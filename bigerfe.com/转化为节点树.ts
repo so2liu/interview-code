3820fe16b423ba13fa51bae8028e17a6892ee290
so2liu <so2liu@gmail.com>
2020-08-16 09:47:56 +0800

    fix: array => node tree

--- bigerfe.com/转化为节点树.ts
+++ bigerfe.com/转化为节点树.ts
@@ -8,10 +8,10 @@ interface SingleNode {
 }
 
 const nodes: SingleNode[] = [
-  { id: 2, name: "2", pId: 0 },
   { id: 4, name: "4", pId: 2 },
-  { id: 12, name: "12", pId: 2 },
+  { id: 2, name: "2", pId: 0 },
   { id: 8, name: "8", pId: 4 },
+  { id: 12, name: "12", pId: 2 },
   { id: 10, name: "10", pId: 4 },
 ];
 
@@ -36,18 +36,28 @@ const expect = {
 function mount(nodes: SingleNode[]) {
   if (nodes.length === 0) return {};
 
-  const cache: { [x: string]: SingleNode } = { [nodes[0].id]: nodes[0] };
+  // 缓存全部cache，顺便找到根节点root
+  const cache: { [x: string]: SingleNode } = {};
+  let root;
+  nodes.forEach((n) => {
+    cache[n.id] = n;
+    if (n.pId === 0) root = n;
+  });
 
-  for (let i = 1; i < nodes.length; i++) {
+  // 挂载节点
+  for (let i = 0; i < nodes.length; i++) {
     const current = nodes[i];
+    if (current === root) continue;
 
     const parent = cache[current.pId];
+
     if (parent.children) parent.children.push(current);
     else parent.children = [current];
 
     cache[current.id] = current;
   }
-  return nodes[0];
+
+  return root;
 }
 
 myTest(mount(nodes), expect);
